11a0572367e0faefd4fbf3e930303995847a3302
Vebs25 <149856036+Vebs25@users.noreply.github.com>
2026-03-21 22:35:55 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-window.myFirebaseConfig = {
-  apiKey: "AIzaSyA6NC2_jH8Ak4FwxjC-8GTQ7G-5jrPMHqg",
-  authDomain: "gpsakhidata.firebaseapp.com",
-  projectId: "gpsakhidata",
-  storageBucket: "gpsakhidata.firebasestorage.app",
-  messagingSenderId: "235936604445",
-  appId: "1:235936604445:web:582fd09db07403382f01f4",
-  measurementId: "G-DKE4F5YRSY"
-};
