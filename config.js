65d961bf6fe9fd0e65ccd90bf6ad5455be8cf5f2
Vebs25 <149856036+Vebs25@users.noreply.github.com>
2026-03-22 08:19:15 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.myFirebaseConfig = {
+  apiKey: "AIzaSyBdSsS6M5VgvozmWwQqocbY1sDuVQWUFTw",
+  authDomain: "gpsakhi-9685e.firebaseapp.com",
+  projectId: "gpsakhi-9685e",
+  storageBucket: "gpsakhi-9685e.firebasestorage.app",
+  messagingSenderId: "107629589245",
+  appId: "1:107629589245:web:7b288fda76b26aaadac494",
+  measurementId: "G-29FM4RM449"
+};
